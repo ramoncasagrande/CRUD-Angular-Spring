bdfe9a5d70eef1cd478dc48e0a8a4318cd98041e
Ramon Casagrande <ramoncasagrande@hotmail.com>
2022-08-29 20:35:57 -0300

    Criado Service courses.service

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,3 +1,4 @@
+import { CoursesService } from './../services/courses.service';
 import { Course } from './../model/course';
 import { Component, OnInit } from '@angular/core';
 
@@ -8,17 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CoursesComponent implements OnInit {
 
-  courses: Course[] = [
-    { _id: '1', name: 'Angular', category: 'Front-End'},
-    { _id: '2', name: 'Spring', category: 'Back-End'}
-  ];
+  courses: Course[] = [];
   displayedColumns = ['name','category'];
 
-  constructor() {
-    //this.courses = []; forma opcional
+  //coursesService: CoursesService;
+
+  constructor(private coursesService: CoursesService) {
+    //this.courses = []; forma opcional --> removido pois foi adicionado o parâmetro
+    //this.coursesService = new CoursesService(); --> removido pois foi adicionado o parâmetro
+    //this.courses = this.coursesService.list(); --> pode ser utilizado no ngOnInit
   }
 
   ngOnInit(): void {
+    coursesService: CoursesService
   }
 
 }
